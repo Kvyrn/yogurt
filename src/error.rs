29f36c5d1f04df1088361f50855b73493168d1f6
Treesoid <70215358+Treesoid@users.noreply.github.com>
2022-06-19 15:07:00 +0200

    reformat

--- src/error.rs
+++ src/error.rs
@@ -1,6 +1,6 @@
-use std::fmt::{Display, Formatter};
 use crate::InvalidCommandReason::UnknownCommand;
 use nom::Err;
+use std::fmt::{Display, Formatter};
 
 #[derive(Debug, Eq, PartialEq)]
 pub enum Error {
